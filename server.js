960489803ab51f25a1402badac978563a7eb90f7
Brent <Bannawit.KH@gmail.com>
2021-10-03 21:29:48 +0700

    make it better3

--- server.js
+++ server.js
@@ -72,13 +72,7 @@ function auth(req, res, next){
     }
 }
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+const path = require("path")
 
 const PORT = process.env.PORT || 3001
 
