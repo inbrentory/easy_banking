ed004608570610a112d7d153fe15cb1d6cd78e39
Brent <Bannawit.KH@gmail.com>
2021-10-03 22:26:52 +0700

    make it better8

--- server.js
+++ server.js
@@ -75,14 +75,13 @@ function auth(req, res, next){
 const path = require("path")
 
 
-if(process.env.NODE_ENV === 'production' === 'production') {
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
     app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-        response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-  
-  }
+}
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3001
 
